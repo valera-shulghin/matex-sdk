5a41a6513ff7528887ca0141c73dda4237bfa966
Maxim Stepanov <max@bearwm.com>
2021-05-19 20:10:30 +0300

    sdk init

---
+++ sdk/src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x2BF881AB3ECC4D607DAdC802DEb40380E9e0DF0c'
+
+export const INIT_CODE_HASH = '0x720dacb1d15426a46163082b8cab8136bf89aceea3055f73ee6097130b381228'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
