4f024e6a07af275383e6efbcd537fe31688ae6bb
Maxim Stepanov <max@bearwm.com>
2021-05-19 21:59:26 +0300

    buidl

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x2BF881AB3ECC4D607DAdC802DEb40380E9e0DF0c";
+export declare const INIT_CODE_HASH = "0x720dacb1d15426a46163082b8cab8136bf89aceea3055f73ee6097130b381228";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
